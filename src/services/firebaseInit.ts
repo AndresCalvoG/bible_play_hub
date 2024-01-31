d95f9ee532fa7bbdca0cd45dba0bed97494a2f2b
AndresCalvoi <andres.calvo@instaleap.io>
2024-01-30 20:24:05 -0500

    feat: add firebase app to the project and git hub action flow

---
+++ src/services/firebaseInit.ts
@@ -0,0 +1,34 @@
+import { Auth, getAuth } from "firebase/auth";
+import firebaseConfig from "./configFirebase";
+import { FirebaseApp, initializeApp } from "firebase/app";
+import { FirebaseStorage, getStorage } from "firebase/storage";
+import { Firestore, getFirestore } from "firebase/firestore";
+import { Messaging, getMessaging } from "firebase/messaging";
+
+let app: FirebaseApp | undefined = undefined;
+let auth: Auth;
+let storage: FirebaseStorage;
+let db: Firestore;
+let messaging: Messaging;
+let isInitialized = false;
+
+async function init(): Promise<boolean> {
+  if (isInitialized) {
+    return true;
+  }
+  try {
+    app = initializeApp(firebaseConfig);
+    // auth = getAuth();
+    // storage = getStorage(app);
+    // db = getFirestore(app);
+    // messaging = getMessaging(app);
+    isInitialized = true;
+    return true;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+}
+init();
+
+export { auth, storage, db, messaging };
