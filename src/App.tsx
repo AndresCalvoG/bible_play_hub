5a499228e8bed5e1213ab544d0b8b1ab794cee22
AndresCalvoG <andrescalvo9407@gmail.com>
2024-05-18 13:07:20 -0500

    feat: add not found page to the router

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import { LoaderUI } from "./components/Loader/LoaderUI";
 import { AppProvider } from "./context";
 import { Layout } from "./components/Layout/Layout";
+import { PrivateRoute } from "./components/PrivateRoute/PrivateRoute";
 
 // const Home = lazy(() => import("./pages/Home/Home"));
 const Landing = lazy(() => import("./pages/Landing/Landing"));
 const Login = lazy(() => import("./pages/Login/Login"));
 const Trivia = lazy(() => import("./pages/Trivia/Trivia"));
+const NotFound = lazy(() => import("./pages/NotFound/NotFound"));
 
 const App = () => {
   return (
@@ -18,7 +20,15 @@ const App = () => {
             <Routes>
               <Route path="/" element={<Landing />} />
               <Route path="/login" element={<Login />} />
-              <Route path="/trivia" element={<Trivia />} />
+              <Route
+                path="/trivia"
+                element={
+                  <PrivateRoute>
+                    <Trivia />
+                  </PrivateRoute>
+                }
+              />
+              <Route path="*" element={<NotFound />} />
             </Routes>
           </Layout>
         </AppProvider>
